24cf295baeab6b5831ed9c21d4290265df9fcc3b
Nicolas Corrarello <ncorrare@gmail.com>
2016-07-20 12:36:34 +0100

    Added base code for ESC domain list

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,22 @@
 class bginfo (
-  String $bgifile = $bginfo::params::bgifile,
-  String $bgipath = $bginfo::params::bgipath,
-  Boolean $setonstart = true,
+  String $bgifile         = $bginfo::params::bgifile,
+  String $bgipath         = $bginfo::params::bgipath,
+  Boolean $setonstart     = true,
+  Boolean $addtrustedsite = true,
 ) inherits bginfo::params {
   #validate_re($bgipath, '^[a-zA-Z]:\\(((?![<>:"\/\\|?*]).)+((?<![ .])\\)?)*.bgi$', 'Confpath should be an absolute Windows Path to a bgi file')
 
+  #Chocolatey will use the underlying libraries of the OS to download packages. So if ESC is enabled, download.sysinternals.com needs to be in that list.
+  if $addtrustedsite {
+    registry_key { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\ESCDomains\download.sysinternals.com':
+      ensure => present,
+    }
+    registry_value { 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\ESCDomains\download.sysinternals.com\https':
+      ensure => present,
+      type   => dword,
+      data   => '0x00000002'
+    }
+  }
   package { 'bginfo':
     ensure   => installed,
     provider => 'chocolatey',
